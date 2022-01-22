b3248e38aaa19e14e14ef0a7472fc2c7745fa7ca
Jaehyun Nam <25205995+kloudmax@users.noreply.github.com>
2022-01-23 00:22:59 +0900

    Delete update_cert.sh

--- update_cert.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CERT=7LzOmG
-
-sudo rm -f server.key
-sudo cp /usr/syno/etc/certificate/_archive/$CERT/privkey.pem server.key
-
-sudo rm -f server.crt
-sudo cp /usr/syno/etc/certificate/_archive/$CERT/fullchain.pem server.crt
